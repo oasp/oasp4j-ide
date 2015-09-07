73dd68caf22ad4f9c0fac4b96789ab35fac149b9
fawinter <fabian.winter@capgemini.com>
2015-09-07 10:40:29 +0200

    Start.bat for oasp
    This is an easy-acess script for oasp. 
    It should copy all essential files from the current project into a new virtual discspace so that the project is ready to be run.

---
+++ oasp4j-ide-extras/Start.bat
@@ -0,0 +1,47 @@
+@echo off
+
+if not exist O:\ (
+subst O: %~dp0.
+) 
+
+if exist O:\START.bat ( 
+rem All good. Starting application.
+	goto :startEclipse
+) else (
+ 	echo WARNING! WARNING! WARNING!
+	echo You're currently using the local disk O:\ for another application please clear the local disk O:\  
+	goto :end
+)
+
+pause
+
+pushd O:\.
+call scripts\environment-project.bat
+
+
+cd workspaces\main\oasp4js
+
+
+call npm install -g gulp
+
+call npm install -g bower
+
+call npm install
+
+cd java
+
+
+call mvn install
+echo Finished installing npm and maven
+
+:startEclipse
+echo oeffnet eclipse-workspace
+call \eclipse-main.bat
+
+:end
+popd
+
+
+
+
+
