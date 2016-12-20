3ef9f04c6cdc8fad2f8af946b7440917d2999a2c
Steffen Holzer <steffen.holzer@capgemini.com>
2016-12-20 17:09:09 +0100

    fixed #117 added remarks to the script, adapted oasp oomph product to use said script

--- oasp4j-ide-oomph-setups/resources/msiinstall.bat
+++ oasp4j-ide-oomph-setups/resources/msiinstall.bat
@@ -1,3 +1,5 @@
 @echo off
+rem %1 name of the msi package in the temp folder without extension
+rem %2 subfolder to be used in software as TARGETDIR. can be empty
 
 msiexec /a temp\%1.msi /qn TARGETDIR="%~dp0software\%2" /L* temp\%1_log.txt
