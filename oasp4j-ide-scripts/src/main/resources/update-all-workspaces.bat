ea0bae07b66801e11dbe4782cd471a282d0bc960
Steffen Holzer <steffen.holzer@capgemini.com>
2017-01-04 17:22:02 +0100

    #118 quoted SOFTWARE_PATH and WORKSPACE_PATH. Seems to fix the problem

--- oasp4j-ide-scripts/src/main/resources/update-all-workspaces.bat
+++ oasp4j-ide-scripts/src/main/resources/update-all-workspaces.bat
@@ -7,7 +7,7 @@ call scripts\environment-project.bat
 set WORKSPACES=%CD%\%WORKSPACES_PATH%
 set UPDATING_ALL_WORKSPACES=true
 
-if not exist %WORKSPACES% (
+if not exist "%WORKSPACES%" (
 	echo Can not find directory: %WORKSPACES%
 	echo Execution aborted
 	goto :end
